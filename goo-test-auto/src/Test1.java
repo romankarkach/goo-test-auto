6d0425024043fbe24161d787247e25b3a86c55c3
nph <nph@nph-System-Product-Name>
2021-03-30 19:50:02 +0300

    Test Suite added with single test for seasrch.

--- goo-test-auto/src/Test1.java
+++ goo-test-auto/src/Test1.java
@@ -1,4 +1,20 @@
+	import static com.codeborne.selenide.Condition.*;
+	import static com.codeborne.selenide.Selenide.*;
+	
+	import org.testng.annotations.Test;
+	import org.openqa.selenium.By;
 
+@Test
 public class Test1 {
+	public void searchGoogle()
+	{
+		open("http://google.com");
+	    $(By.name("q")).val("test")
+	    	.pressEnter();
 
+	    $("#ires li.g").shouldBe(visible)
+	    	.shouldHave(text("Test"));
+
+	    screenshot("google_results");
+	 }
 }
